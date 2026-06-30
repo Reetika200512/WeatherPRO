30de6b83f37f7fc87d6c557fdbb4673bc703de25
Reetika Bhuyan <reetikabhuyan2005@gmail.com>
2026-07-01 01:09:01 +0530

    Remove exposed weather API key

--- WeatherPro/js/config.js
+++ WeatherPro/js/config.js
@@ -2,7 +2,7 @@ export const CONFIG = {
   apiBaseV25: 'https://api.openweathermap.org/data/2.5',
   oneCallBase: 'https://api.openweathermap.org/data/3.0/onecall',
   geoBase: 'https://api.openweathermap.org/geo/1.0',
-  apiKey: '744a9dc20cbb1112283fded84f4ff3fb',
+  apiKey: 'YOUR_OPENWEATHERMAP_API_KEY',
   defaultUnits: 'metric',
   defaultLang: 'en',
   suggestionLimit: 5,
