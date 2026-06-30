6ef0aa1ff0e3f91b3ce6d980e4b301df2d23e726
Reetika Bhuyan <reetikabhuyan2005@gmail.com>
2026-07-01 01:21:20 +0530

    Make weather config local-only

--- WeatherPro/js/config.js
+++ WeatherPro/js/config.js
@@ -1,8 +1,17 @@
+let localConfig = {};
+
+try {
+  const module = await import('./config.local.js');
+  localConfig = module?.CONFIG || module?.default || module || {};
+} catch {
+  localConfig = {};
+}
+
 export const CONFIG = {
   apiBaseV25: 'https://api.openweathermap.org/data/2.5',
   oneCallBase: 'https://api.openweathermap.org/data/3.0/onecall',
   geoBase: 'https://api.openweathermap.org/geo/1.0',
-  apiKey: 'YOUR_OPENWEATHERMAP_API_KEY',
+  apiKey: localConfig.apiKey || 'YOUR_OPENWEATHERMAP_API_KEY',
   defaultUnits: 'metric',
   defaultLang: 'en',
   suggestionLimit: 5,
